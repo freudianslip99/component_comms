e4876c396092ac7051979fb7278d0c8e004367e6
Alan <alan.tnpsych@gmail.com>
2018-06-28 11:59:18 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,8 +1,15 @@
 const inputComponent = {
   template: `<input
           :placeholder="placeholder"
+          v-model="input"
+          @keyup.enter="monitorEnterKey"
           class="input is-small" type="text" />`,
-  props:['placeholder']
+  props:['placeholder'],
+  data () {
+    return {
+      input: ''
+    }
+  }    
 }
 
 new Vue({
